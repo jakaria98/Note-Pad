6d8fd1ad9a39cb0bc892aef180e8e5917f9f9237
Jakaria <jhossain439@gmail.com>
2025-11-13 12:21:11 -0330

    few error fixed and database migration done

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -6,6 +6,5 @@ app_name = "api"
 urlpatterns = [
     # class-based views
     path('notes/', views.NoteListCreate.as_view(), name='note-list'),
-    path('notes/update/<int:pk>/', views.NoteUpdate.as_view(), name='note-update'),
-    path('notes/delete/<int:pk>/', views.NoteDelete.as_view(), name='note-delete'),
+    path('notes/update/<int:pk>/', views.NoteDetail.as_view(), name='note-detail'),
 ]
